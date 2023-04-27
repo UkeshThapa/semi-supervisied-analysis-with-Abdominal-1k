dc77574d095af9766c2cd4f8104535368e8b8932
UkeshThapa <yukeshthapa8@gmail.com>
2023-04-27 12:48:24 +0545

    write the train function

--- utilities.py
+++ utilities.py
@@ -1,53 +1,51 @@
 import torch
+from tqdm import tqdm
 from monai.inferers import sliding_window_inference
+from monai.data import decollate_batch
 from monai.transforms import (
     AsDiscrete,
     Compose
 )
 
 
-# Hello this is new 
 
-def train(model,device,):
-    max_epochs = 10
-    val_interval = 2
+
+def train(exp_num,model,device,max_epochs,val_interval,num_segments,data,loss_function,optimizer,dice_metric):
+    
+    # data from the dataloader
+    train_loader,val_loader = data
+
+    # variable to store the best metric and best epoch
     best_metric = -1
     best_metric_epoch = -1
+    
     epoch_loss_values = []
     metric_values = []
-    post_pred = Compose([AsDiscrete(argmax=True, to_onehot=5)])
-    post_label = Compose([AsDiscrete(to_onehot=5)])
+
+
+    post_pred = Compose([AsDiscrete(argmax=True, to_onehot=num_segments)])
+    post_label = Compose([AsDiscrete(to_onehot=num_segments)])
 
     for epoch in range(max_epochs):
-        print("-" * 10)
-        print(f"epoch {epoch + 1}/{max_epochs}")
         model.train()
         epoch_loss = 0
         step = 0
-        for batch_data in train_loader:
+        for i,batch_data in enumerate(tqdm(train_loader)):
             step += 1
             inputs, labels = (
                 batch_data["image"].to(device),
                 batch_data["label"].to(device),
             )
             optimizer.zero_grad()
-            # print(n))
-            print(inputs.shape)
-            print("Size of inputs :", inputs.shape)
-            print("Size of inputs[0] :", inputs[0].shape)
             outputs = model(inputs)
-            print("hello")
+
             loss = loss_function(outputs, labels)
             loss.backward()
             optimizer.step()
             epoch_loss += loss.item()
-            print(
-                f"{step}/{len(train_ds) // train_loader.batch_size}, "
-                f"train_loss: {loss.item():.4f}")
         epoch_loss /= step
         epoch_loss_values.append(epoch_loss)
-        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
-
+        tqdm.write(f"Epoch {epoch + 1}, Batch {i + 1}/{len(train_loader)}, Average Loss: {epoch_loss:.4f}")
         if (epoch + 1) % val_interval == 0:
             model.eval()
             with torch.no_grad():
@@ -56,30 +54,36 @@ def train(model,device,):
                         val_data["image"].to(device),
                         val_data["label"].to(device),
                     )
-                    roi_size = (160, 160, 160)
+                    roi_size = (64, 64, 64)
                     sw_batch_size = 4
                     val_outputs = sliding_window_inference(
                         val_inputs, roi_size, sw_batch_size, model)
                     val_outputs = [post_pred(i) for i in decollate_batch(val_outputs)]
                     val_labels = [post_label(i) for i in decollate_batch(val_labels)]
-                    # compute metric for current iteration
+        #             # compute metric for current iteration
                     dice_metric(y_pred=val_outputs, y=val_labels)
 
-                # aggregate the final mean dice result
+        #         # aggregate the final mean dice result
                 metric = dice_metric.aggregate().item()
-                print("dice score: ",metric)
-                # reset the status for next validation round
                 dice_metric.reset()
 
                 metric_values.append(metric)
                 if metric > best_metric:
                     best_metric = metric
                     best_metric_epoch = epoch + 1
-                    torch.save(model.state_dict(), os.path.join(
-                        dir, "best_metric_model.pth"))
-                    print("saved new best metric model")
-                print(
-                    f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
-                    f"\nbest mean dice: {best_metric:.4f} "
-                    f"at epoch: {best_metric_epoch}"
-                )
+                    state ={
+                        'epoch' : best_metric_epoch,
+                        'state_dict' : model.state_dict(),
+                        'optimizer' : optimizer.state_dict(),
+                        'best_metric' : best_metric
+
+                    }
+                    torch.save(state, f"model_experiment_{exp_num}.pth.tr")
+                    
+                    print(
+                        "\nSaved new best metric model"
+                        f"\nBest mean dice: {best_metric:.4f} "
+                        f"\nAt epoch: {best_metric_epoch}\n"
+                        )
+
+
