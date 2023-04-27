a285b2419c696955546005c1c91553e504315f5d
UkeshThapa <yukeshthapa8@gmail.com>
2023-04-27 14:44:23 +0545

    add resume feature in training

--- utilities.py
+++ utilities.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from tqdm import tqdm
 from monai.inferers import sliding_window_inference
@@ -10,26 +11,36 @@ from monai.transforms import (
 
 
 
-def train(exp_num,model,device,max_epochs,val_interval,num_segments,data,loss_function,optimizer,dice_metric):
+def train(checkpoint_path, exp_num,model,device,max_epochs,val_interval,num_segments,data,loss_function,optimizer,dice_metric):
     
     # data from the dataloader
     train_loader,val_loader = data
-
-    # variable to store the best metric and best epoch
-    best_metric = -1
-    best_metric_epoch = -1
-    
     epoch_loss_values = []
     metric_values = []
-
-
     post_pred = Compose([AsDiscrete(argmax=True, to_onehot=num_segments)])
     post_label = Compose([AsDiscrete(to_onehot=num_segments)])
 
-    for epoch in range(max_epochs):
+# resume feature for training
+    if checkpoint_path:
+        checkpoint = torch.load(checkpoint_path)
+        best_metric = checkpoint['best_metric']
+        best_metric_epoch = checkpoint['epoch']
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        
+    else:
+        start_epoch = 0
+        # variable to store the best metric and best epoch
+        best_metric = -1
+        best_metric_epoch = -1
+
+
+    for epoch in range(start_epoch,max_epochs):
         model.train()
         epoch_loss = 0
         step = 0
+        print(optimizer.state_dict())
         for i,batch_data in enumerate(tqdm(train_loader)):
             step += 1
             inputs, labels = (
@@ -78,12 +89,14 @@ def train(exp_num,model,device,max_epochs,val_interval,num_segments,data,loss_fu
                         'best_metric' : best_metric
 
                     }
-                    torch.save(state, f"model_experiment_{exp_num}.pth.tr")
+
+                    if not os.path.exists('result'):
+                        os.makedirs('result')
+
+                    torch.save(state, f".\\result\model_experiment_{exp_num}.pth.tr")
                     
                     print(
                         "\nSaved new best metric model"
                         f"\nBest mean dice: {best_metric:.4f} "
                         f"\nAt epoch: {best_metric_epoch}\n"
                         )
-
-
